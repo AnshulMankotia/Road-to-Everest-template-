d6551a57c2ef3163c398615b3731764e014c8f79
anshul <anshulmankotia1997@gmail.com>
2022-12-27 17:34:26 +0530

    add menu bar

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-// function menuBar(){ 
-// let menuBar=document.getElementById('menu-bar')
-// menuBar.style.backgroundColor="red"
-// console.log(menuBar)
-// }
 
 const menuBar=document.querySelector('#menu-bar');
 const navLinks=document.querySelector('.nav-links');
+const navLinks2=document.querySelector('#nav-links2');
+const header=document.querySelector('header');
+const section=document.querySelector('.section');
 
 menuBar.addEventListener("click" , ()=>{
     menuBar.classList.toggle('active')
     navLinks.classList.toggle('active')
+    navLinks2.classList.toggle('active')
+    header.classList.toggle('active')
+    section.classList.toggle('active')
 });
+
